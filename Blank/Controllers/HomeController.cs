d56a39b8fb354905048f5c1f5a0d82f984481e89
Victor Felix <vfelix82@gmail.com>
2018-02-20 08:35:41 -0800

    add missing home directory in views folder.

--- Blank/Controllers/HomeController.cs
+++ Blank/Controllers/HomeController.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Name.Models;
 
-
 namespace WordCounter.Controllers
 {
     public class HomeController : Controller
     {
-
+        [HttpGet("/")]
+        public ActionResult Index(){
+        return View();
     }
 
 }
